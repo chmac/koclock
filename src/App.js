3f48ae34c557eee414231f16a942f5fce60f8730
Callum Macdonald <git@callum-macdonald.com>
2019-04-27 00:03:20 +0200

    Basic 7 segment with redux state. Not yet wired.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Provider } from "react-redux";
+// import SevenSegmentDisplay from "react-seven-segment-display";
+import Display from "seven-segment-display";
+import store from "./store";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <div className="Display">
+          {/* <SevenSegmentDisplay value={3} /> */}
+          <Display value="1204" digitCount={4} />
+        </div>
+      </div>
+    </Provider>
   );
 }
 
---
+++ src/store.js
@@ -0,0 +1,40 @@
+import { createStore, combineReducers } from "redux";
+
+const TICK = "TICK";
+const tick = () => ({
+  type: TICK,
+  payload: {
+    now: Math.floor(Date.now() / 1e3 / 60),
+    random: Math.floor(Math.random() * 1440) - 720
+  }
+});
+
+const empty = {
+  time: 0,
+  show: 0
+};
+
+const reducer = (state = empty, action) => {
+  const { type, payload } = action;
+
+  if (type === TICK) {
+    const { now, random } = payload;
+    return {
+      time: now,
+      show: now + random
+    };
+  }
+
+  return state;
+};
+
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+export default store;
+
+window.tick = () => {
+  store.dispatch(tick());
+};
